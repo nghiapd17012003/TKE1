66e1464ed2ae69450605e7d420f50df38450e091
nghiapd17012003 <45303883+nghiapd17012003@users.noreply.github.com>
2022-12-13 13:42:32 +0700

    13/12/2022
    1:42pm

--- tke/Elevator.cs
+++ tke/Elevator.cs
@@ -11,13 +11,29 @@ namespace tke
     {
         public List<Button> insideEleButton;
         public List<Button> cabinPosition;
+        public Button positionScreen;
+        public Button directionScreen;
+        public Button elevatorDoor;
+        public Button closeDoor;
+        public Button openDoor;
+        public Button status;
+        public Button mode;
+        public Button connectingStatus;
+        public TextBox eventCapturer;
 
         public Elevator()
         {
             insideEleButton = new List<Button>();
             cabinPosition = new List<Button>();
-        }
-
-        
+            positionScreen = new Button();
+            directionScreen = new Button();
+            elevatorDoor = new Button();
+            closeDoor = new Button();
+            openDoor = new Button();
+            status = new Button();
+            mode = new Button();
+            connectingStatus = new Button();
+            eventCapturer = new TextBox();
+        }      
     }
 }
--- tke/Form1.cs
+++ tke/Form1.cs
@@ -9,16 +9,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
-
 namespace tke
 {
     public partial class Form1 : Form
     {
-        int height = 8;
+        int height = 10;
         int noOfElevator = 4;
         List<Button> upButtonList = new List<Button>();
         List<Button> downButtonList = new List<Button>();
-        List<Button> positionScreen = new List<Button>();
         List<Elevator> elevatorList = new List<Elevator>();
 
         public Form1()
@@ -33,9 +31,14 @@ namespace tke
             {
                 Button singleUp = new Button();
                 singleUp.Image = Image.FromFile(@"C:\Users\nghia\OneDrive\Desktop\project\TKE1\tke\image\upArrow.png");
-                singleUp.Location = new Point(0, 40 * i); 
+                singleUp.Location = new Point(0, 40 * i);
                 singleUp.Size = new Size(37, 30);
-                p.Controls.Add(singleUp);   
+                if (i == 0)
+                {
+                    singleUp.Visible = false;
+                }
+
+                p.Controls.Add(singleUp);
                 upButtonList.Add(singleUp);
             }
         }
@@ -48,10 +51,32 @@ namespace tke
                 singleDown.Image = Image.FromFile(@"C:\Users\nghia\OneDrive\Desktop\project\TKE1\tke\image\downArrow.png");
                 singleDown.Location = new Point(47, 40 * i);
                 singleDown.Size = new Size(37, 30);
+                if (i == height - 1)
+                {
+                    singleDown.Visible = false;
+                }
                 p.Controls.Add(singleDown);
                 downButtonList.Add(singleDown);
             }
         }
+
+        private void levelLable(Panel p)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                Label level = new Label();
+                level.Text = "" + (height -i);
+                level.Location = new Point(94, 40 * i);
+                level.Size = new Size(37, 30);
+                level.TextAlign = (System.Drawing.ContentAlignment)HorizontalAlignment.Center;
+                level.TextAlign = (System.Drawing.ContentAlignment)System.Windows.Forms.VisualStyles.VerticalAlignment.Bottom;
+               
+                
+                p.Controls.Add(level);              
+            }
+        }
+
+
 
         public void addControlPanel()
         {
@@ -61,6 +86,7 @@ namespace tke
             controlPanel.BorderStyle = BorderStyle.FixedSingle;
             upButton(controlPanel);
             downButton(controlPanel);
+            levelLable(controlPanel);
 
             Controls.Add(controlPanel);
         }
@@ -70,34 +96,69 @@ namespace tke
             Elevator elevator = new Elevator();
             Panel elevatorPanel = new Panel();
             int elevatorePanelHeight = height * 40;
-            if (elevatorePanelHeight < 200)
+            if (elevatorePanelHeight < 270)
             {
-                elevatorePanelHeight += 200;//make sure if the height is <= 2, there will be enough space for other components
+                elevatorePanelHeight += 270;//make sure if the height is <= 2, there will be enough space for other components
             }
-            elevatorPanel.Size = new Size(250, elevatorePanelHeight);
-            elevatorPanel.Location = new Point(240 + elevatorNo * 270, 0);//control panel width + control panel left = 200 + 20 = 220, elevator panel width + space between each panel = 250 + 20 = 270;
+            elevatorPanel.Size = new Size(300, elevatorePanelHeight);
+            elevatorPanel.Location = new Point(240 + elevatorNo * 320, 0);//control panel width + control panel left = 200 + 20 = 220, elevator panel width + space between each panel = 300 + 20 = 320;
             elevatorPanel.BorderStyle = BorderStyle.Fixed3D;
 
-            Button positionScreen = new Button();
-            positionScreen.Location = new Point(94, 0);
-            positionScreen.Size = new Size(100, 50);
-            positionScreen.BackColor = Color.Black;
-            positionScreen.Text = "1";
-            positionScreen.ForeColor = Color.White;
-            elevatorPanel.Controls.Add(positionScreen);
+            
+            elevator.positionScreen.Location = new Point(94, 0);
+            elevator.positionScreen.Size = new Size(50, 50);
+            elevator.positionScreen.BackColor = Color.Black;
+            elevator.positionScreen.Text = "1";
+            elevator.positionScreen.Font = new Font("Broadway", 12);
+            elevator.positionScreen.ForeColor = Color.White;
+            elevatorPanel.Controls.Add(elevator.positionScreen);
 
-            Button directionScreen = new Button();
-            directionScreen.Location = new Point(194, 0);
-            directionScreen.Size = new Size(50, 50);
-            directionScreen.BackColor = Color.Black;
-            directionScreen.Image = Image.FromFile(@"C:\Users\nghia\OneDrive\Desktop\project\TKE1\tke\image\upArrowInWhite.png");
-            elevatorPanel.Controls.Add(directionScreen);
+            elevator.directionScreen.Location = new Point(144, 0);          
+            elevator.directionScreen.Size = new Size(50, 50);
+            elevator.directionScreen.BackColor = Color.Black;
+            elevator.directionScreen.Image = Image.FromFile(@"C:\Users\nghia\OneDrive\Desktop\project\TKE1\tke\image\upArrowInWhite.png");
+            elevatorPanel.Controls.Add(elevator.directionScreen);
 
-            Button elevatorDoor = new Button();
-            elevatorDoor.Location = new Point(94, 60);
-            elevatorDoor.Size = new Size(100, 140);
-            elevatorDoor.Image = Image.FromFile(@"C:\Users\nghia\OneDrive\Desktop\project\TKE1\tke\image\closeDoor.png");
-            elevatorPanel.Controls.Add(elevatorDoor);
+            elevator.elevatorDoor.Location = new Point(94, 60);
+            elevator.elevatorDoor.Size = new Size(100, 140);
+            elevator.elevatorDoor.Image = Image.FromFile(@"C:\Users\nghia\OneDrive\Desktop\project\TKE1\tke\image\closeDoor.png");
+            elevatorPanel.Controls.Add(elevator.elevatorDoor);
+
+            elevator.closeDoor.Location = new Point(244, 60);
+            elevator.closeDoor.Size = new Size(50, 50);
+            elevator.closeDoor.Image = Image.FromFile(@"C:\Users\nghia\OneDrive\Desktop\project\TKE1\tke\image\closeDoorButton.png");
+            elevatorPanel.Controls.Add(elevator.closeDoor);
+
+            elevator.openDoor.Location = new Point(194, 60);
+            elevator.openDoor.Size = new Size(50, 50);
+            elevator.openDoor.Image = Image.FromFile(@"C:\Users\nghia\OneDrive\Desktop\project\TKE1\tke\image\openDoorButton.png");
+            elevatorPanel.Controls.Add(elevator.openDoor);
+
+            elevator.status.Location = new Point(244, 0);
+            elevator.status.Size = new Size(50, 50);
+            elevator.status.Text = "Stop";
+            elevator.status.BackColor = Color.Black;
+            elevator.status.ForeColor = Color.White;
+            elevatorPanel.Controls.Add(elevator.status);
+
+            elevator.mode.Location = new Point(194, 0);
+            elevator.mode.Size = new Size(50, 50);
+            elevator.mode.Text = "Priority";
+            elevator.mode.BackColor = Color.White;
+            elevatorPanel.Controls.Add(elevator.mode);
+
+            elevator.connectingStatus.Location = new Point(194, 120);
+            elevator.connectingStatus.Size = new Size(100, 80);
+            elevator.connectingStatus.Text = "Disconected";
+            elevator.connectingStatus.BackColor = Color.White;
+            elevatorPanel.Controls.Add(elevator.connectingStatus);
+
+            elevator.eventCapturer.Location = new Point(94, 200);
+            elevator.eventCapturer.Size = new Size(200, 100);
+            elevator.eventCapturer.Multiline = true;
+            elevator.eventCapturer.ScrollBars = ScrollBars.Both;
+            elevator.eventCapturer.Text = "Status:";
+            elevatorPanel.Controls.Add(elevator.eventCapturer);
 
             for (int i = 0; i < height; i++)
             {
@@ -130,7 +191,5 @@ namespace tke
                 newElevator(i);
             }
         }
-
-
     }
 }
